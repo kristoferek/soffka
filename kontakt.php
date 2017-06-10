ceb8e751c8c495e1de930ce663a0b15d0b5a9573
Krzysztof.Nowak <krzysztof.michal.nowak@gmail.com>
2017-06-10 22:54:36 +0200

    Contact form

---
+++ kontakt.php
@@ -0,0 +1,38 @@
+<section class="contact-us">
+	<ul class="accordion" data-accordion>
+		<li class="accordion-item is-active" data-accordion-item>
+			<a href="#" class="accordion-title">Bepłatna rozmowa</a>
+			<div class="accordion-content free-call-description" data-tab-content>
+				<div class="small-12 medium-10 coliumns">
+					<p>Będąc w trudnej stytacji związanej ze zdradą każdy z nas potrzbuje poukładać sobie byśli i emocje tak, by móc podjąć ważne dla siebie decyzje i dalej funkcjonować w rodzinie, czy pracy. Zdecydowałam się podarować każdej potrzebującej osobie swój czas i zaangażowanie.
+					<p class="callout primary">We <strong>wtorki i czwartki między godziną 16.00, a 20.00</strong> mogę w wolnym terminie umówić się Tobą na bezpłatną rozmowę telefoniczną <strong> trwającą 15 minut</strong>.</p>
+					<p>Prześlij mi swój nr telefonu komórkowego, a ja zaproponuję Ci najpliższy wolny termin i prześlę potwierdzenie w wiadomości sms.
+
+					<form class="free-call-form" action="mailto:kontakt@soffka.pl" method="post" data-abide novalidate >
+						<label>Twój numer telefonu
+							<input type="text" placeholder="Nr telefonu - np.:500 000 000" aria-describedby="Wpisz numer telefonu" required pattern="number"><span class="form-error">Numer telefonu może zawierać wyłącznie cyfry.</span>
+						</label>
+						<input type="submit" class="button" value="Wyślij" />
+					</form>
+				</div>
+			</div>
+		</li>
+		<li  class="accordion-item" data-accordion-item>
+			<a href="#" class="accordion-title">Formularz kontaktowy</a>
+			<div class="accordion-content" data-tab-content>
+				<p class="contact-form-description">Użyj formularza by wysłać wiadomość</p>
+				<form class="contact-form" action="mailto:kontakt@soffka.pl" method="post">
+					<div class="input-group">
+						<span class="input-group-label">Imię:</span><input class="input-group-field" type="text" placeholder="Tu wpisz swoje imię">
+					</div>
+					<div class="input-group">
+						<span class="input-group-label">mail:</span><input class="input-group-field" type="email" placeholder="Tu wpisz poprawny adres email">
+					</div>
+					<textarea name="message" id="" rows="12" placeholder="Treść wiadomości"></textarea>
+					<input type="submit" class="button" value="Wyślij" />
+				</form>
+			</div>
+		</li>
+		<!-- ... -->
+	</ul>
+</section>
